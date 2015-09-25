b22a5a6cbc496bf33684ff3ddb170d7ee7b826df
FabianHeiss <fabian.heiss@sew-eurodrive.de>
2015-09-25 14:06:49 +0200

    Layout 1.2

--- FBE2.MaXolution.Fertigungsplanung/ViewModel/HauptfensterViewModel.cs
+++ FBE2.MaXolution.Fertigungsplanung/ViewModel/HauptfensterViewModel.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Microsoft.Practices.Prism.Commands;
 using FBE2.MaXolution.Fertigungsplanung.View;
 using MahApps.Metro.Controls;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
 
 namespace FBE2.MaXolution.Fertigungsplanung.ViewModel
 {
@@ -14,8 +16,56 @@ namespace FBE2.MaXolution.Fertigungsplanung.ViewModel
     {
         public HauptfensterViewModel()
         {
-            
+            this.AddViews();
+            this.ActiveView = new AuftragslisteView();
         }
 
+        #region ViewControl
+        private DelegateCommand<string> OpenView{ get; set; }
+        private ObservableCollection<UserControl> Views = new ObservableCollection<UserControl>();
+        
+        private void OpenView_Execute(string ViewName)
+        {
+            if (Views.Any(p => p.Name == ViewName))
+            {
+                UserControl View = Views.First(p => p.Name == ViewName);
+                ActiveView = View;
+            }
+        }
+
+        private bool OpenView_CanExecute(string ViewName)
+        {
+            if (Views.Any(p => p.Name == ViewName))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private UserControl _ActiveView;
+        public UserControl ActiveView
+        {
+            get { return _ActiveView; }
+            set
+            {
+                if (_ActiveView == value)
+                    return;
+
+                _ActiveView = value;
+                OnPropertyChanged("ActiveView");
+
+            }
+        }
+
+        private void AddViews()
+        {
+            // Views hinzufügen
+            // Views.Add(new ViewName());
+            Views.Add(new AuftragslisteView());
+        }
+        #endregion
     }
 }
