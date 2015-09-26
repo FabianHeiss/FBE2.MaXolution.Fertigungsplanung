de17db4cc01698079f58317e3a6f2b9a98c5d831
FabianHeiss <fabian.heiss@sew-eurodrive.de>
2015-09-26 20:07:12 +0200

    Layoout 0.3 and Function 0.1

--- FBE2.MaXolution.Fertigungsplanung/Model/Auftrag.cs
+++ FBE2.MaXolution.Fertigungsplanung/Model/Auftrag.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using FBE2.MaXolution.Fertigungsplanung.Framework;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,9 +25,33 @@ namespace FBE2.MaXolution.Fertigungsplanung.Model
 
         #region Funktionen
         // Auftrag aus Datenbank holen und Klassenparameter mit Daten füllen
-        private void getAuftrag(long Id)
+        public void getAuftrag(long Id)
         {
             // ToDo: Parameter mit Werten füllen
+            Datenbank db = new Datenbank();
+            DataTable dt = new DataTable();
+            dt = db.ExecuteQuery("SELECT * FROM S_Auftrag WHERE Auftrag_Id = " + Id.ToString());
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                foreach (DataColumn dc in dt.Columns){
+                    var cellContent = dr[dc];
+                    switch (dc.ColumnName)
+                    {
+                        case "Auftragsnummer":
+                            Auftragsnummer = cellContent.ToString();
+                            break;
+                        case "Gerät_Id":
+                            Gerät_Id = (long)(int)cellContent;
+                            break;
+                        case "Fertigungsstatus_Id":
+                            Fertigungsstatus_Id = (long)(int)cellContent;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
         }
 
         // Auftrag in Datenbank speichern / updaten
@@ -46,7 +72,12 @@ namespace FBE2.MaXolution.Fertigungsplanung.Model
 
         #region Klassenparameter
         // Allgemeine Felder
-
+        public long Auftrag_Id { get; set; }
+        public string Auftragsnummer { get; set; }
+        public long Gerät_Id { get; set; }
+        public Geraet Gerät { get; set; }
+        public long Fertigungsstatus_Id { get; set; }
+        public Fertigungsstatus Fertigungsstatus { get; set; }
 
         // Begleitkarten-Felder
 
---
+++ FBE2.MaXolution.Fertigungsplanung/ViewModel/AuftragslisteViewModel.cs
@@ -0,0 +1,26 @@
+﻿using FBE2.MaXolution.Fertigungsplanung.Framework;
+using FBE2.MaXolution.Fertigungsplanung.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FBE2.MaXolution.Fertigungsplanung.ViewModel
+{
+    class AuftragslisteViewModel : BaseViewModel
+    {
+        public AuftragslisteViewModel()
+        {
+            _Auftragsliste.Add(new Auftrag(2));
+        }
+
+        private ObservableCollection<Auftrag> _Auftragsliste = new ObservableCollection<Auftrag>();
+
+        public ObservableCollection<Auftrag> Auftragsliste
+        {
+            get { return _Auftragsliste; }
+        }
+    }
+}
